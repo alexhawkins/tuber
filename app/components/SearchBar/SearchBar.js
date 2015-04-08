b17a5fdb5f07b3eb5ea56084c3cc91ca2152780c
Alex Hawkins <alexhawkins.me@gmail.com>
2015-04-07 20:44:49 -0700

    changes made to search component

--- app/components/SearchBar/SearchBar.js
+++ app/components/SearchBar/SearchBar.js
@@ -6,30 +6,30 @@ var SearchBar = React.createClass({
     mixins: [Router.Navigation],
     handleSubmit: function(){
         var query = this.refs.query.getDOMNode().value.trim(); // remove trailing white space
-        query = query.replace(/\s+/g, "-"); //add dash if spaces search
+        query = query.replace(/\s+/g, "-"); //add dash btw spaces
         this.refs.query.getDOMNode().value = "";
-        if(query !== "") // don"t search if nothing has been entered
+        if(query !== "") // skip transition if query blank
             this.transitionTo("search", {query: query});
     },
     render: function(){
-            return (
-                <form onSubmit={this.handleSubmit}>
-                    <div className="input-group">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Search..."
-                            aria-describedby="sizing-addon2"
-                            ref="query"
-                        />
-                        <span className="input-group-btn">
-                            <button
-                                type="submit"
-                                className="btn btn-default search-button">Video Search!</button>
-                        </span>
-                    </div>
-                </form>
-            );
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="input-group">
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Search..."
+                        aria-describedby="sizing-addon2"
+                        ref="query"
+                    />
+                    <span className="input-group-btn">
+                        <button
+                            type="submit"
+                            className="btn btn-default search-button">Video Search!</button>
+                    </span>
+                </div>
+            </form>
+        );
     }
 
 });
